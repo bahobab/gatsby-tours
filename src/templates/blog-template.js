2fa8e49649fe4f66aa7479e5e61e877fd5cafc39
mono motapa <bahobab@hotmail.com>
2020-04-16 22:14:55 -0500

    added code to handle embedded images in post

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { graphql, useStaticQuery } from "gatsby"
+import { graphql } from "gatsby"
 import AniLink from "gatsby-plugin-transition-link/AniLink"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 import { Layout } from "../components/Layout"
 import styles from "../css/blog-single.module.css"
@@ -12,12 +13,29 @@ const Blog = ({ data }) => {
     text: { json },
   } = data.post
 
+  const options = {
+    renderNode: {
+      "embedded-asset-block": node => {
+        return (
+          <div className="rich">
+            <h3>This is an Awesome image</h3>
+            <img width="400" src={node.data.target.fields.file["en-US"].url} />
+            <p>Image provided by john doe</p>
+          </div>
+        )
+      },
+    },
+  }
+
   return (
     <Layout>
       <section className={styles.blog}>
         <div className={styles.center}>
           <h1>{title}</h1>
           <h4>published at: {published}</h4>
+          <article className={styles.post}>
+            {documentToReactComponents(json, options)}
+          </article>
           <AniLink fade to="/blog" className="btn-primary">
             all posts
           </AniLink>
