41db5f72500ce4541e86f6848c9dbacd621f5304
mono motapa <bahobab@hotmail.com>
2020-04-16 21:32:55 -0500

    created blog template and blog pages

---
+++ src/templates/blog-template.js
@@ -0,0 +1,42 @@
+import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+
+import { Layout } from "../components/Layout"
+import styles from "../css/blog-single.module.css"
+
+const Blog = ({ data }) => {
+  const {
+    title,
+    published,
+    text: { json },
+  } = data.post
+
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <div className={styles.center}>
+          <h1>{title}</h1>
+          <h4>published at: {published}</h4>
+          <AniLink fade to="/blog" className="btn-primary">
+            all posts
+          </AniLink>
+        </div>
+      </section>
+    </Layout>
+  )
+}
+
+export default Blog
+
+export const getPost = graphql`
+  query getPost($slug: String) {
+    post: contentfulPost(slug: { eq: $slug }) {
+      title
+      published(formatString: "MMMM Do, YYYY")
+      text {
+        json
+      }
+    }
+  }
+`
